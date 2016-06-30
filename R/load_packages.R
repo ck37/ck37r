98e9b49a42bd40d688766f93719957cffa2cf014
Chris Kennedy <chrisken@gmail.com>
2016-06-30 14:22:55 -0700

    Add load_packages.R

---
+++ R/load_packages.R
@@ -0,0 +1,38 @@
+#' Load a list of packages.
+#'
+#' Load packages and install them from CRAN if they aren't already available.
+#'
+#' @param pkgs Character vector of packages to load.
+#' @param auto_install Install any packages that could not be loaded.
+#'
+#' @importFrom utils capture.output install.packages
+#'
+#' @export
+load_packages = function(pkgs, auto_install = F) {
+
+  # Try to load each package, and save whether or not it succeeded.
+  capture.output({ result = sapply(pkgs, require, character.only=T, quietly=T) })
+
+  install_result = NULL
+  result_retry = NULL
+
+  # Return a helpful message and the install.packages command if needed.
+  if (sum(!result) > 0) {
+    cat("These packages need to be installed:", paste(pkgs[!result], collapse=", "), "\n")
+    install_code = paste0('install.packages(c("', paste(pkgs[!result], collapse='", "'), '"))')
+    cat(install_code, "\n")
+
+    if (auto_install) {
+      cat("Auto-installing from repository:", getOption("repos")[1], "\n")
+      install_result = install.packages(pkgs[!result])
+      # Try to load newly installed packages.
+      capture.output({ result_retry = sapply(pkgs[!result], require, character.only=T, quietly=T) })
+    }
+  } else {
+    install_code = ""
+  }
+
+  results = list(packages = pkgs, pkgs_result = result, pkgs_retry = result_retry,
+                 install_code = install_code)
+  invisible(result)
+}
