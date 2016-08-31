6f50143512592549228ee5198d24fe1379a2b1ad
Chris Kennedy <chrisken@gmail.com>
2016-08-31 09:44:03 -0700

    Need mode() for impute_missing_values

---
+++ R/Mode.R
@@ -0,0 +1,8 @@
+#' Compute the mode of a vector (can be multiple results).
+# Via http://stackoverflow.com/questions/2547402/is-there-a-built-in-function-for-finding-the-mode
+Mode = function(x) {
+  ux = unique(x)
+  tab <- tabulate(match(x, ux))
+  # This will return multiple modes if they exist.
+  ux[tab == max(tab)]
+}
