557beee02a4698188478f48c95e5c86057873d16
Chris Kennedy <chrisken@gmail.com>
2016-11-06 16:05:48 -0800

    Support tibbles in impute_missing_indicators()

--- R/impute_missing_values.R
+++ R/impute_missing_values.R
@@ -12,8 +12,11 @@ impute_missing_values = function(data, add_indicators = T, prefix = "miss_",
                                  skip_vars = c(), verbose = F) {
   # Loop over each feature.
   missing_indicators = NULL
-  for (i in 1:ncol(data)){
-    nas = sum(is.na(data[, i]))
+
+  # TODO: vectorize, and support parallelization.
+  for (i in 1:ncol(data)) {
+    # Use double brackets rather than [, i] to support tibbles.
+    nas = sum(is.na(data[[i]]))
     # Nothing to impute, continue to next column.
     # TODO: add note and also skip if nas are 100% of the data.
     if (nas == 0 || names(data)[i] %in% skip_vars) {
@@ -25,24 +28,24 @@ impute_missing_values = function(data, add_indicators = T, prefix = "miss_",
       next
     }
 
-    missing_indicator = matrix(1 * is.na(data[, i]))
+    missing_indicator = matrix(1 * is.na(data[[i]]))
     new_name = paste0(prefix, colnames(data)[i])
     colnames(missing_indicator) = new_name
     next_col = ncol(missing_indicators) + 1
     if (is.null(missing_indicators)) {
-      missing_indicators = matrix(missing_indicator, nrow=nrow(data), ncol=1)
+      missing_indicators = matrix(missing_indicator, nrow = nrow(data), ncol = 1)
       colnames(missing_indicators) = new_name
     } else {
       missing_indicators = cbind(missing_indicators, missing_indicator)
     }
 
-    if (class(data[, i]) == "factor") {
+    if (class(data[[i]]) == "factor") {
       # Impute factors to the mode.
       # Choose the first mode in case of ties.
-      data[is.na(data[, i]), i] = Mode(data[, i])[1]
+      data[is.na(data[[i]]), i] = Mode(data[[i]])[1]
     } else {
       # Impute numeric values to the median.
-      data[is.na(data[, i]), i] = median(data[, i], na.rm=T)
+      data[is.na(data[[i]]), i] = median(data[[i]], na.rm = T)
     }
   }
   if (add_indicators) {
