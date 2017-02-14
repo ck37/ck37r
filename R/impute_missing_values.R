809853e095fa3227fd45bff32176e46c5b459bd9
Chris Kennedy <chrisken@gmail.com>
2017-02-14 13:47:24 -0800

    Use missingness_indicators() in impute_missing_values()

--- R/impute_missing_values.R
+++ R/impute_missing_values.R
@@ -1,5 +1,5 @@
 #' Impute missing values in a dataframe and add missingness indicators.
-#' @description tbd
+#' @description Median-impute numerics, Mode-impute factors, add missingness indicators.
 #' @param data Dataframe or matrix.
 #' @param add_indicators Add a series of missingness indicators.
 #' @param prefix String to add at the beginning of the name of each missingness
@@ -13,6 +13,9 @@ impute_missing_values = function(data, add_indicators = T, prefix = "miss_",
                                  skip_vars = c(), verbose = F) {
   # Loop over each feature.
   missing_indicators = NULL
+
+  # Make a copy to store the imputed dataframe.
+  new_data = data
 
   # TODO: vectorize, and support parallelization.
   for (i in 1:ncol(data)) {
@@ -29,28 +32,25 @@ impute_missing_values = function(data, add_indicators = T, prefix = "miss_",
       next
     }
 
-    missing_indicator = matrix(1 * is.na(data[[i]]))
-    new_name = paste0(prefix, colnames(data)[i])
-    colnames(missing_indicator) = new_name
-    next_col = ncol(missing_indicators) + 1
-    if (is.null(missing_indicators)) {
-      missing_indicators = matrix(missing_indicator, nrow = nrow(data), ncol = 1)
-      colnames(missing_indicators) = new_name
-    } else {
-      missing_indicators = cbind(missing_indicators, missing_indicator)
-    }
-
     if (class(data[[i]]) == "factor") {
       # Impute factors to the mode.
       # Choose the first mode in case of ties.
-      data[is.na(data[[i]]), i] = Mode(data[[i]])[1]
+      new_data[is.na(data[[i]]), i] = Mode(data[[i]])[1]
     } else {
       # Impute numeric values to the median.
-      data[is.na(data[[i]]), i] = median(data[[i]], na.rm = T)
+      new_data[is.na(data[[i]]), i] = median(data[[i]], na.rm = T)
     }
   }
   if (add_indicators) {
-    data = cbind(data, missing_indicators)
+    # Create missingness indicators from original dataframe.
+    missing_indicators = missingness_indicators(data, prefix = prefix, verbose = verbose)
+
+    if (verbose) {
+      cat("Indicators added:", ncol(missing_indicators), "\n")
+    }
+
+    # Append indicators.
+    new_data = cbind(new_data, missing_indicators)
   }
-  data
+  new_data
 }
