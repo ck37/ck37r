433dde4dcb2b1ec452d514ea552a7695c2952fb0
Chris Kennedy <chrisken@gmail.com>
2017-02-14 16:28:24 -0800

    Add randomForest getTree import

--- R/rf_count_terminal_nodes.R
+++ R/rf_count_terminal_nodes.R
@@ -1,12 +1,13 @@
 #' Count the terminal nodes in each tree from a random forest
 #' @param rf Random Forest object
 #' @return vector of terminal node counts
-#' @examples
-#'
+# @examples
+#
 # TODO: flesh out example
 # terminal_nodes = rf_count_terminal_nodes(rf)
 # summary(terminal_nodes)
 #' @export
+#' @importFrom randomForest getTree
 rf_count_terminal_nodes = function(rf) {
   terminal_nodes = rep(NA, rf$forest$ntree)
 
