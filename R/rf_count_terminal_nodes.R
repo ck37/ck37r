809d58bd1d8a6df9089c5add43d0bd13d5f158b9
Chris Kennedy <chrisken@gmail.com>
2017-02-14 16:21:39 -0800

    Fix rf_count_terminal_nodes for labelVar = F

--- R/rf_count_terminal_nodes.R
+++ R/rf_count_terminal_nodes.R
@@ -15,8 +15,9 @@ rf_count_terminal_nodes = function(rf) {
     # Extract a single tree from the forest.
     tree = randomForest::getTree(rf, tree_i, labelVar = F)
 
-    # Terminal nodes have NA as their split variable.
-    sum_na = sum(is.na(tree$`split var`))
+    # Terminal nodes have 0 as their split variable.
+    # NOTE: if we turn labelVar on, split variables with have NAs instead of 0s.
+    sum_na = sum(tree[, "split var"] == 0)
 
     terminal_nodes[tree_i] = sum_na
   }
