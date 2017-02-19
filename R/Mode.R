7c01a6c93aa1c0bc439dc2ec8f9652b1eb846a2c
Chris Kennedy <chrisken@gmail.com>
2017-02-19 13:35:51 -0800

    Mode: add test and example

--- R/Mode.R
+++ R/Mode.R
@@ -1,10 +1,27 @@
-#' Compute the mode of a vector (can be multiple results).
+#' @title Compute the mode of a vector (can be multiple results).
+#'
+#' @description
+#'
+#' Via http://stackoverflow.com/questions/2547402/is-there-a-built-in-function-for-finding-the-mode
+#'
 #' @param x vector
+#'
+#' @return Vector of modal values in arbitrary order.
+#'
+#' @examples
+#'
+#' library(ck37r)
+#'
+#' data(Boston, package = "MASS")
+#'
+#' table(Boston$chas)
+#'
+#' Mode(Boston$chas)
+#'
 #' @export
-# Via http://stackoverflow.com/questions/2547402/is-there-a-built-in-function-for-finding-the-mode
 Mode = function(x) {
   ux = unique(x)
-  tab <- tabulate(match(x, ux))
+  tab = tabulate(match(x, ux))
   # This will return multiple modes if they exist.
   ux[tab == max(tab)]
 }
---
+++ tests/testthat/test_Mode.R
@@ -0,0 +1,7 @@
+library(ck37r)
+
+data(Boston, package = "MASS")
+
+table(Boston$chas)
+
+Mode(Boston$chas)
