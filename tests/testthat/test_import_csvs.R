afb2b4577948336bf7686b2c05f0f1b30136f254
Chris Kennedy <chrisken@gmail.com>
2017-03-11 10:52:16 -0800

    Fix working directory for test_import_csvs

--- tests/testthat/test_import_csvs.R
+++ tests/testthat/test_import_csvs.R
@@ -1,11 +1,13 @@
 library(ck37r)
 
-# Note: when run as an automated check this will work, but manually it won't due to inst/extdata.
-files = import_csvs("extdata", verbose = T)
-
-# This version is for testing manually.
-if (F) {
-  files = import_csvs("inst/extdata", verbose = T)
+# During devtools::test() working directory is tests/testthat.
+if (dir.exists("../../inst/extdata")) {
+  test_dir = "../../inst/extdata"
+} else {
+  # When run manually working directory will be main package directory.
+  test_dir = "inst/extdata"
 }
+
+files = import_csvs(test_dir, verbose = T)
 
 names(files)
