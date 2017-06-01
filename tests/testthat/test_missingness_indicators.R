8a832eac9b0a70000371a15a7b4884d6cb5a91b8
Chris Kennedy <chrisken@gmail.com>
2017-05-31 21:57:20 -0700

    include verbose missingness indicators test

--- tests/testthat/test_missingness_indicators.R
+++ tests/testthat/test_missingness_indicators.R
@@ -5,7 +5,8 @@ data(PimaIndiansDiabetes2, package = "mlbench")
 colSums(is.na(PimaIndiansDiabetes2))
 
 # Generate missingness indicators; skip outcome variable.
-indicators = missingness_indicators(PimaIndiansDiabetes2, skip_vars = "diabetes")
+indicators = missingness_indicators(PimaIndiansDiabetes2,
+                                    skip_vars = "diabetes", verbose = T)
 
 # Check missingness.
 colSums(indicators)
