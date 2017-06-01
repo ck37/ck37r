81e1520708f7e489c7b9333b1d6cf6237d2b3576
Chris Kennedy <chrisken@gmail.com>
2017-05-31 21:52:39 -0700

    Add more load_packages tests and rename test file

---
+++ tests/testthat/test-load_packages.R
@@ -0,0 +1,19 @@
+library(ck37r)
+library(testthat)
+
+context("test_load_packages")
+
+# Load these 4 packages but don't install any.
+load_packages(c("MASS", "SuperLearner", "tmle", "doParallel"))
+
+# Load packages and update installed packages.
+load_packages(c("MASS"), update = T)
+
+# Load these 4 packages and install them if necessary.
+load_packages(c("MASS", "SuperLearner", "tmle", "doParallel"), auto_install = TRUE)
+
+# Try to install a non-existent package.
+# This will generate warnings.
+suppressWarnings(try({
+  load_packages("ck37_blah123", auto_install = T)
+}))
