bcc5817d306288fe21de0c324e5e424d3e14b2c6
Chris Kennedy <chrisken@gmail.com>
2017-06-01 00:01:20 -0700

    Add extra import_csvs test

--- tests/testthat/test-import_csvs.R
+++ tests/testthat/test-import_csvs.R
@@ -1,4 +1,5 @@
 library(ck37r)
+library(testthat)
 
 # During devtools::test() working directory is tests/testthat.
 if (dir.exists("../../inst/extdata")) {
@@ -9,5 +10,13 @@ if (dir.exists("../../inst/extdata")) {
 }
 
 files = import_csvs(test_dir, verbose = T)
+
+names(files)
+
+# Go up one directory, which should be empty.
+# This will generate a warning.
+suppressWarnings({
+  files = import_csvs(paste0(test_dir, "../"), verbose = T, recursive = F)
+})
 
 names(files)
