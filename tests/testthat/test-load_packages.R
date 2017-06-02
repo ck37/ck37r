6684e1d3a92f362c043557595b7198377d9d210e
Chris Kennedy <chrisken@gmail.com>
2017-06-01 22:05:18 -0700

    Re-enable some load_packages tests

--- tests/testthat/test-load_packages.R
+++ tests/testthat/test-load_packages.R
@@ -7,17 +7,13 @@ context("test_load_packages")
 load_packages(c("MASS", "SuperLearner", "tmle", "doParallel"))
 
 # Load packages and update installed packages.
-#load_packages(c("MASS"), update = T)
+# load_packages(c("MASS"), update = T)
 
 # Load these 4 packages and install them if necessary.
-if (F) {
-load_packages(c("MASS", "SuperLearner", "tmle", "doParallel"), auto_install = TRUE)
-}
+load_packages(c("MASS", "SuperLearner", "tmle", "doParallel"), auto_install = T)
 
 # Try to install a non-existent package.
 # This will generate warnings.
-if (F) {
 suppressWarnings(try({
   load_packages("ck37_blah123", auto_install = T)
 }))
-}
