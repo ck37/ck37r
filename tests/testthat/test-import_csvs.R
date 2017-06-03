1e40afe482d054d81096a7e450f86e3c7b7c5dd0
Chris Kennedy <chrisken@gmail.com>
2017-06-03 12:29:46 -0700

    Tweak import-csvs test

--- tests/testthat/test-import_csvs.R
+++ tests/testthat/test-import_csvs.R
@@ -4,6 +4,9 @@ library(testthat)
 # During devtools::test() working directory is tests/testthat.
 if (dir.exists("../../inst/extdata")) {
   test_dir = "../../inst/extdata"
+} else if (dir.exists("../../extdata")) {
+  # After package is installed inst/extdata will be moved to extdata.
+  test_dir = "../../extdata"
 } else {
   # When run manually working directory will be main package directory.
   test_dir = "inst/extdata"
@@ -16,7 +19,7 @@ names(files)
 # Go up one directory, which should be empty.
 # This will generate a warning.
 suppressWarnings({
-  files = import_csvs(paste0(test_dir, "../"), verbose = T, recursive = F)
+  files = import_csvs(paste0(test_dir, "/../"), verbose = T, recursive = F)
 })
 
 names(files)
