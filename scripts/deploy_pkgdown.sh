9861dc05ac9911b8226396f7f264374310073ad2
Chris Kennedy <chrisken@gmail.com>
2017-06-03 17:17:10 -0700

    Ignore removal in git add

--- scripts/deploy_pkgdown.sh
+++ scripts/deploy_pkgdown.sh
@@ -65,7 +65,7 @@ fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
+git add . --ignore-removal
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
