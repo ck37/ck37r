1e4625501c7dc6c83cef07947cbb0c98d2e9496e
Chris Kennedy <chrisken@gmail.com>
2017-06-25 13:25:16 -0700

    impute_missing_values: improve test coverage

--- tests/testthat/test-impute_missing_values.R
+++ tests/testthat/test-impute_missing_values.R
@@ -6,13 +6,28 @@ context("Impute missing values")
 # Load a test dataset.
 data(PimaIndiansDiabetes2, package = "mlbench")
 
+data = PimaIndiansDiabetes2
+
 # Check for missing values.
-colSums(is.na(PimaIndiansDiabetes2))
+colSums(is.na(data))
+
+# Convert day_of_week to a factor
+data$day_of_week = as.factor(data$day_of_week)
+
+# Add some missing values.
+data$day_of_week[1:3] = NA
+
+# Convert visibility to a character for testing purposes.
+data$visibility = as.character(data$visibility)
+# Add include some missingness.
+data$visibility[5:10] = NA
 
 # Impute missing data and add missingness indicators.
 # Don't impute the outcome though.
-result = impute_missing_values(PimaIndiansDiabetes2, skip_vars = "diabetes",
-                               verbose = T)
+# This will generate warnings due to data$visibility.
+suppressWarnings({
+  result = impute_missing_values(data, skip_vars = "diabetes", verbose = T)
+})
 
 # Confirm we have no missing data.
 colSums(is.na(result$data))
