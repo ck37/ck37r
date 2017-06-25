b69dd760cb96d99b70d37fcee6da9eac62442bc0
Chris Kennedy <chrisken@gmail.com>
2017-06-25 14:49:13 -0700

    try to get load_all_code test working

--- tests/testthat/test-load_all_code.R
+++ tests/testthat/test-load_all_code.R
@@ -1,3 +1,13 @@
 library(ck37r)
+library(testthat)
 
-load_all_code("R")
+context("load all code")
+
+print(getwd())
+
+# If running via devtools::check() or test(), working dir will be tests/testthat.
+if (dir.exists("R")) {
+  load_all_code("R", verbose = T)
+} else {
+  load_all_code("../../R", verbose = T)
+}
