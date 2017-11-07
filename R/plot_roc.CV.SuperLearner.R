0a49efb50cb5b76fea200055394e27e887b5396c
Chris Kennedy <chrisken@gmail.com>
2017-11-07 17:53:18 -0600

    plot_roc.CV.SuperLearner: plot best learner, not necessarily SuperLearner; also allow selecting a certain learner

--- R/plot_roc.CV.SuperLearner.R
+++ R/plot_roc.CV.SuperLearner.R
@@ -4,6 +4,7 @@
 #'
 #' @param x CV.SuperLearner object
 #' @param y Outcome vector if not already included in the SL object.
+#' @param learner Which learner to plot - defaults to highest AUC learner.
 #' @param title Title to use in the plot.
 #' @param subtitle Optional plot subtitle.
 #' @param digits Digits to use when rounding AUC and CI for plot.
@@ -55,25 +56,77 @@
 #' @method plot_roc CV.SuperLearner
 #'
 #' @export
-plot_roc.CV.SuperLearner = function(x, y = x$Y,
-                         title = "CV-SuperLearner cross-validated ROC",
-                         subtitle = "",
-                         digits = 4,
-                         show_plot = TRUE,
-                         ...) {
+plot_roc.CV.SuperLearner =
+  function(x, y = x$Y,
+           learner = NULL,
+           title = "CV-SuperLearner cross-validated ROC",
+           subtitle = NULL,
+           digits = 4,
+           show_plot = TRUE,
+           ...) {
   # Change to a better object name.
   cvsl = x
 
-  preds = cvsl$SL.predict
-  pred = ROCR::prediction(preds, y)
-  perf1 = ROCR::performance(pred, "sens", "spec")
+  auc_table = ck37r::auc_table(cvsl, y)
 
-  ciout = ck37r::cvsl_auc(cvsl)
+  cvsl_table = summary(cvsl)$Table
+
+  # Choose the learner with the highest AUC.
+  # TODO: support passing in strings "SuperLearner" or "DiscreteSL" rather than
+  # an index.
+  if (is.null(learner)) {
+    # Note: multiple learners may have the best AUC.
+    best_auc = max(auc_table$auc, na.rm = TRUE)
+
+    # First extract the learner name, then later get the index.
+    learner_name = rownames(auc_table)[auc_table$auc == best_auc]
+
+    if (length(learner_name) > 1) {
+      # TODO: sort by SE first, so that we take the learner with the lower SE.
+      # (Although generally any AUC ties will have the same SE.)
+
+      # If there are multiple winners and SuperLearner is in there, use SL.
+      # Otherwise use DiscreteSL, otherwise use the first learner name.
+      if ("SuperLearner" %in% learner_name) {
+        learner_name = "SuperLearner"
+      } else if ("DiscreteSL" %in% learner_name) {
+        learner_name = "DiscreteSL"
+      } else {
+        learner_name = learner_name[1]
+      }
+    }
+
+    # Extract the original learner index based on learner name in the AUC table.
+    learner = which(cvsl_table$Algorithm == learner_name)
+  } else {
+    learner_name = as.character(cvsl_table$Algorithm[learner])
+  }
+
+  if (learner_name == "SuperLearner") {
+    preds = cvsl$SL.predict
+  } else if (learner_name == "DiscreteSL") {
+    preds = cvsl$discreteSL.predict
+  } else {
+    # library.predict contains only the raw learners, not SL or DiscreteSL.
+    preds = cvsl$library.predict[, learner - 2]
+  }
+
+  # Set learner name as the subtitle if no subtitle was specified.
+  if (is.null(subtitle)) {
+    subtitle = paste("Best learner:", learner_name)
+  }
+
+  auc = auc_table[learner_name, "auc"]
+  ci_upper = auc_table[learner_name, "ci_upper"]
+  ci_lower = auc_table[learner_name, "ci_lower"]
 
   txt = paste0("AUC = ",
-               sprintf(paste0("%0.", digits, "f"), round(ciout$cvAUC, digits)),
-               "\n95% CI = ", sprintf("%0.3f", round(ciout$ci[1], digits)),
-               " - ", sprintf("%0.3f", round(ciout$ci[2], digits)))
+               sprintf(paste0("%0.", digits, "f"), round(auc, digits)),
+               "\n95% CI = ", sprintf("%0.3f", round(ci_lower, digits)),
+               " - ", sprintf("%0.3f", round(ci_upper, digits)))
+
+  pred = ROCR::prediction(preds, y)
+  perf1 = ROCR::performance(pred, "sens", "spec")
 
   # ggplot version.
   p = ggplot2::qplot(1 - methods::slot(perf1, "x.values")[[1]],
@@ -92,9 +145,9 @@ plot_roc.CV.SuperLearner = function(x, y = x$Y,
   }
 
   # Return AUC, AUC CI, and ggplot object.
-  results = list(auc = ciout$cvAUC,
-                 auc_se = ciout$se,
-                 auc_ci = ciout$ci,
+  results = list(auc = auc,
+                 auc_se = auc_table[learner_name, "se"],
+                 auc_ci = c(ci_lower, ci_upper),
                  plot = p)
 
   # Return results invisibly.
