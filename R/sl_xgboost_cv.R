5eb6e980c395de78637097972f0eaad0a4fd789a
Chris Kennedy <chrisken@gmail.com>
2018-06-10 15:55:10 -0700

    sl_xgboost_cv: add integrated support for gaussian family

--- R/sl_xgboost_cv.R
+++ R/sl_xgboost_cv.R
@@ -1,5 +1,5 @@
 # TODO: PR for SuperLearner
-# TODO: soup up family = gaussian() / integrate with family = binomial()
+# TODO: test gaussian family
 # TOOD: add example
 #' XGBoost SuperLearner wrapper with internal cross-validation for early-stopping
 #'
@@ -66,12 +66,14 @@ SL.xgboost_cv =
            colsample_bytree = 0.8,
            gamma = 5,
            # Due to class imbalance,
-           stratified = T,
+           stratified = family$family == "binomial",
            # Focus on AUC and see if we do better.
            eval_metric = ifelse(family$family == "binomial", "auc", "rmse"),
            print_every_n = 400L,
-           nthread = 1,
-           verbose = 0, save_period = NULL, ...) {
+           nthread = getOption("sl.cores", 1L),
+           verbose = 0,
+           save_period = NULL, ...) {
+
     #.SL.require("xgboost")
     if (utils::packageVersion("xgboost") < 0.6)
       stop("SL.xgboost requires xgboost version >= 0.6, try help('SL.xgboost') for details")
@@ -84,47 +86,51 @@ SL.xgboost_cv =
     xgmat = xgboost::xgb.DMatrix(data = X, label = Y, weight = obsWeights)
 
     if (family$family == "gaussian") {
-      model = xgboost::xgboost(data = xgmat, objective = "reg:linear",
-                               nrounds = ntrees, max_depth = max_depth, min_child_weight = minobspernode,
-                               eta = shrinkage, verbose = verbose, nthread = nthread,
-                               save_period = save_period)
+      objective = "reg:linear"
+    } else if (family$family == "binomial") {
+      objective = "binary:logistic"
     }
-    if (family$family == "binomial") {
-      model_cv =
-        xgboost::xgb.cv(data = xgmat, objective = "binary:logistic",
-                        nrounds = ntrees,
-                        max_depth = max_depth,
-                        min_child_weight = minobspernode,
-                        eta = shrinkage,
-                        verbose = 1L,
-                        nthread = nthread,
-                        subsample = subsample,
-                        colsample_bytree = colsample_bytree,
-                        gamma = gamma,
-                        early_stopping_rounds = early_stopping_rounds,
-                        stratified = stratified,
-                        print_every_n = print_every_n,
-                        metrics = list(eval_metric),
-                        nfold = nfold,
-                        save_period = save_period)
 
+    model_cv =
+      xgboost::xgb.cv(data = xgmat, objective = objective,
+                      nrounds = ntrees,
+                      max_depth = max_depth,
+                      min_child_weight = minobspernode,
+                      eta = shrinkage,
+                      verbose = verbose,
+                      nthread = nthread,
+                      subsample = subsample,
+                      colsample_bytree = colsample_bytree,
+                      gamma = gamma,
+                      early_stopping_rounds = early_stopping_rounds,
+                      stratified = stratified,
+                      print_every_n = print_every_n,
+                      metrics = list(eval_metric),
+                      nfold = nfold,
+                      save_period = save_period)
+
+    if (verbose) {
       print(model_cv)
-
-      # Refit that model using the best params.
-      model =
-        xgboost::xgboost(data = xgmat, #objective = "binary:logistic",
-                         nrounds = model_cv$best_ntreelimit,
-                         params = model_cv$params,
-                         verbose = 0L,
-                         print_every_n = 500L,
-                         save_period = save_period)
     }
+
+    # Refit that model using the best params.
+    model =
+      xgboost::xgboost(data = xgmat,
+                       objective = objective,
+                       nrounds = model_cv$best_ntreelimit,
+                       params = model_cv$params,
+                       verbose = 0L,
+                       print_every_n = 500L,
+                       save_period = save_period)
+
     if (family$family == "multinomial") {
+      # Not used yet.
       model = xgboost::xgboost(data = xgmat, objective = "multi:softmax",
                                nrounds = ntrees, max_depth = max_depth, min_child_weight = minobspernode,
                                eta = shrinkage, verbose = verbose, num_class = length(unique(Y)),
                                nthread = nthread, save_period = save_period)
     }
+
     if (!is.matrix(newX)) {
       newX = stats::model.matrix(~ ., newX)[, -1]
     }
