24d1757a556ba19a0693f7bf40593d5d5c8378d8
Chris Kennedy <chrisken@gmail.com>
2020-11-25 10:19:01 -0500

    Add plot_table()

---
+++ R/plot_table.R
@@ -0,0 +1,26 @@
+#' Plot a table of values using ggplot
+#' @param x Table of ML results, assuming to have "learner", "ci_lower", and "ci_upper" columns.
+#' @param metric Column to plot
+#' @param sort If TRUE (default), sort rows based on the metric.
+#'
+#' @export
+plot_table = function(x,
+                      metric = "auc",
+                      sort = TRUE) {
+  # Use a clearer object name.
+  tab = x
+  if (!is.null(sort)) {
+    tab = tab[order(tab[[metric]], decreasing = sort), ]
+  }
+  # Convert to a factor with manual levels so ggplot doesn't re-order
+  # alphabetically.
+  tab$learner = factor(tab$learner, levels = tab$learner)
+  rownames(tab) = NULL
+  p =
+    ggplot2::ggplot(tab,
+                    aes(x = learner, y = get(metric), ymin = ci_lower, ymax = ci_upper)) +
+    ggplot2::geom_pointrange(fatten = 2) +
+    ggplot2::coord_flip() +
+    ggplot2::labs(x = "Learner", y = metric) + theme_minimal()
+  return(p)
+}
